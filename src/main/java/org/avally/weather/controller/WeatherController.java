0fb9443eef8734b4c332aadd1b06bec604d4627a
Dmitry Polishchuk <dim.polishchuk@gmail.com>
2021-08-09 19:02:04 +0300

    configure weather endpoint

--- src/main/java/org/avally/weather/controller/WeatherController.java
+++ src/main/java/org/avally/weather/controller/WeatherController.java
@@ -1,5 +1,8 @@
 package org.avally.weather.controller;
 
+import org.avally.weather.model.Summary;
+import org.avally.weather.service.WeatherApiService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,9 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class WeatherController {
+    private final WeatherApiService weatherApiService;
+
+    public WeatherController(WeatherApiService weatherApiService) {
+        this.weatherApiService = weatherApiService;
+    }
 
     @GetMapping("/weather")
-    public String getWeatherByCity(@RequestParam("city") String city) {
-        return null;
+    public ResponseEntity<Summary> getWeatherByCity(@RequestParam("city") String city) {
+        ResponseEntity<Summary> apiResponse = weatherApiService.getWeatherFromApi(city);
+        if (apiResponse.getStatusCode().is2xxSuccessful() && apiResponse.hasBody()) {
+            Summary summary = apiResponse.getBody();
+            return ResponseEntity.ok(summary);
+        }
+        return ResponseEntity.badRequest().build();
     }
 }
