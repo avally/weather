27fb96665b86240a07c15a30912f1cd2ac3f5db7
Dmitry Polishchuk <dim.polishchuk@gmail.com>
2021-08-09 20:11:59 +0300

    save data into DB

--- src/main/java/org/avally/weather/controller/WeatherController.java
+++ src/main/java/org/avally/weather/controller/WeatherController.java
@@ -1,6 +1,7 @@
 package org.avally.weather.controller;
 
 import org.avally.weather.model.Summary;
+import org.avally.weather.repository.WeatherRepository;
 import org.avally.weather.service.WeatherApiService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +9,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/api")
 public class WeatherController {
     private final WeatherApiService weatherApiService;
+    private final WeatherRepository weatherRepository;
 
-    public WeatherController(WeatherApiService weatherApiService) {
+    public WeatherController(WeatherApiService weatherApiService, WeatherRepository weatherRepository) {
         this.weatherApiService = weatherApiService;
+        this.weatherRepository = weatherRepository;
     }
 
     @GetMapping("/weather")
@@ -22,7 +27,9 @@ public class WeatherController {
         ResponseEntity<Summary> apiResponse = weatherApiService.getWeatherFromApi(city);
         if (apiResponse.getStatusCode().is2xxSuccessful() && apiResponse.hasBody()) {
             Summary summary = apiResponse.getBody();
-            return ResponseEntity.ok(summary);
+//            Because we don`t have any criteria to fetch data from DB, just saving 'As is'
+            Summary savedSummary = weatherRepository.save(Objects.requireNonNull(summary));
+            return ResponseEntity.ok(savedSummary);
         }
         return ResponseEntity.badRequest().build();
     }
